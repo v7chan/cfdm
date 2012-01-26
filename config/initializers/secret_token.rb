db5c6f15ada1e8dba01b393409f8981dfcc68229
Vincent Chan <c_vchan@qualcomm.com>
2012-01-25 16:26:38 -0800

    Testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cfdm::Application.config.secret_token = 'b0309a67c7e72c03610e3519d956c9f5a90af875f4e4ae7ae5ee8de31df4f166400c81e72fdc464ec9fa50b534c80fcb4ecc7583734535576591d36b06b8ca3b'
