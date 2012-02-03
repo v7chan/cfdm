06e284165675d2c2a58a4273e53dd145732c356a
Vincent Chan <c_vchan@qualcomm.com>
2012-02-03 10:16:51 -0800

    Fixed 0 bug, added db

--- app/controllers/wod_list_controller.rb
+++ app/controllers/wod_list_controller.rb
@@ -13,9 +13,11 @@ class WodListController < ApplicationController
     
     @tomorrows_date = doc.xpath('//div[3]/a//text()')
     
-    tomorrows_list = doc.xpath('//div[4]/table//text()')
+    tomorrows_list = doc.xpath('//div[@id="tomorrowwod"]/table//text()')
     @tomorrows_strength_skill = tomorrows_list[1]
-    @tomorrows_wod = todays_list[3..(tomorrows_list.length - 1)]
+    @tomorrows_wod = tomorrows_list[3..(tomorrows_list.length - 1)]
+    
+    @debug = todays_list
     
   end
 end
