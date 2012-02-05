d96d9fef1f5cd07c23807f41940d303cfe34bdc1
Vincent Chan <c_vchan@qualcomm.com>
2012-02-05 14:55:13 -0800

    Fixed edge case for Sunday

--- app/controllers/wod_list_controller.rb
+++ app/controllers/wod_list_controller.rb
@@ -6,18 +6,24 @@ class WodListController < ApplicationController
     doc = Nokogiri::HTML(open("http://www.crossfitdelmar.com/"))
     
     @todays_date = doc.xpath('//h3//text()')
-   
+    @tomorrows_date = doc.xpath('//div[3]/a//text()')
+    
     todays_list = doc.xpath('//div[2]/table//text()')
+    tomorrows_list = doc.xpath('//div[@id="tomorrowwod"]/table//text()')
+    
+    if @todays_date.empty?
+      @todays_date = 'No WOD Today :('
+    end
+    
     @todays_strength_skill = todays_list[1]
     @todays_wod = todays_list[3..(todays_list.length - 1)]
     
-    @tomorrows_date = doc.xpath('//div[3]/a//text()')
+    if (@tomorrows_date.empty? && tomorrows_list.any?)
+      @tomorrows_date = doc.xpath('//div[2]/a[@class="alt"]//text()')
+    end
     
-    tomorrows_list = doc.xpath('//div[@id="tomorrowwod"]/table//text()')
     @tomorrows_strength_skill = tomorrows_list[1]
     @tomorrows_wod = tomorrows_list[3..(tomorrows_list.length - 1)]
-    
-    @debug = todays_list
     
   end
 end
