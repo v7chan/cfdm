9012539a6f052509a41717b8ae3aac719ec2d31c
Vincent Chan <vince.chan@housecallpro.com>
2024-01-25 14:58:20 -0800

    Create foo

---
+++ db/migrate/20210813024732_create_foos.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateFoos < ActiveRecord::Migration
+  def change
+    create_table :foos do |t|
+      t.string :bar
+
+      t.timestamps null: false
+    end
+  end
+end
