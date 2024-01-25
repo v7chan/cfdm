70b58ad318e8a04c9f8397565b33f3a8b2d0d124
Vincent Chan <vince.chan@housecallpro.com>
2024-01-25 15:45:57 -0800

    Test changes on release branch

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,10 @@
 
 ActiveRecord::Schema.define(:version => 0) do
 
+  create_table "foos", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "bars", limit: 191
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
